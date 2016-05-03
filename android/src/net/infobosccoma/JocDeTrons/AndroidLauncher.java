c76b27468a4ee9679cf699e219bbce0184491538
David <visies_12@hotmail.com>
2016-05-03 18:30:47 +0200

    Comentari

--- android/src/net/infobosccoma/JocDeTrons/AndroidLauncher.java
+++ android/src/net/infobosccoma/JocDeTrons/AndroidLauncher.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
+
+	//COMENTARI
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
